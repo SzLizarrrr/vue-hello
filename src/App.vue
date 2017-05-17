135b2ed3027ca4e561101f230f606c4f375d9d4d
SzLizarrrr <yf8189@gmail.com>
2017-05-17 14:25:19 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <header></header>
+    <div class="tab">
+      I am Tab!
+    </div>
+
+    <div class="content">
+      I am Content!
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import header from './components/header/header'
+
+  export default {
+    components: {
+      header: header
+    }
+  }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

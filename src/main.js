00f2fffb04ad3c559b51fb8f3536a29480ca2851
SzLizarrr <yf8189@gmail.com>
2017-05-17 22:22:39 -0700

    router working

--- src/main.js
+++ src/main.js
@@ -2,10 +2,24 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import goods from './components/goods/goods'
+
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
+const router = new VueRouter({
+  routes: [
+    {path: '/goods', component: goods}
+  ]
+})
+
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
   components: {App}
 })
+
+// const app = new Vue({router}).$mount('#app')
+

8013f7de338ff3b87aeaa8b2601ff9da646c3214
SzLizarrrr <yf8189@gmail.com>
2017-05-21 04:45:51 -0700

    starting goods page modify

--- src/components/title/title.vue
+++ src/components/title/title.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="title">
+  <div class="detail-title">
     <div class="line"></div>
     <div class="text">{{content}}</div>
     <div class="line"></div>
@@ -18,7 +18,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  .title
+  .detail-title //was using "title", but conflict with header.vue's title css
     display: flex
     width: 80%
     margin: 28px auto 24px auto

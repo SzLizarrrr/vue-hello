0f81f7f6335c88f40b04b6a371834b07de772011
szlizarrrr <yf8189@gmail.com>
2017-11-02 20:18:42 -0700

    v-for with key bind

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}"
+        <li v-for="(item,index) in goods" :key="index" class="menu-item" :class="{'current':currentIndex===index}"
             @click="selectMenu(index,$event)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
@@ -12,10 +12,10 @@
     </div>
     <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list" ref="foodList">
+        <li v-for="(item, index) in goods" :key="index" class="food-list" ref="foodList">
           <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food-item border-1px">
+            <li v-for="(food, index) in item.foods" :key="index" class="food-item border-1px">
               <div class="icon">
                 <img width="57" height="57" :src="food.icon"/>
               </div>
@@ -26,8 +26,7 @@
                   <span class="count">{{food.sellCount}} selled per month.</span><span>Rate{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="money-sign">￥</span><span class="new">{{food.price}}</span><span v-show="food.oldPrice"
-                                                                                                class="old">￥{{food.oldPrice}}</span>
+                  <span class="money-sign">￥</span><span class="new">{{food.price}}</span><span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -39,171 +38,218 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import BScroll from 'better-scroll'
+import BScroll from "better-scroll";
 
-  const ERR_OK = 0
+const ERR_OK = 0;
 
-  export default {
-    props: {
-      seller: {
-        type: Object
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      goods: [],
+      listHeight: [],
+      scrollY: 0
+    };
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i;
+        }
       }
-    },
-    data() {
-      return {
-        goods: [],
-        listHeight: [],
-        scrollY: 0
+      return 0;
+    }
+  },
+  created() {
+    this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
+
+    this.$http.get("/api/goods").then(response => {
+      response = response.body;
+      if (response.errno === ERR_OK) {
+        this.goods = response.data;
+        this.$nextTick(() => {
+          this._initScroll();
+          this._calculateHeight();
+        });
       }
+    });
+  },
+  methods: {
+    // when I create at the first time, I typed "method", then browser keep complain about "_initScroll is not a function"
+    selectMenu(index, event) {
+      if (!event._constructed) {
+        return;
+      }
+      let foodList = this.$refs.foodList;
+      let el = foodList[index];
+      this.foodsScroll.scrollToElement(el, 300);
     },
-    computed: {
-      currentIndex() {
-        for (let i = 0; i < this.listHeight.length; i++) {
-          let height1 = this.listHeight[i]
-          let height2 = this.listHeight[i + 1]
-          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
-            return i
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+        click: true
+      });
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        click: true,
+        probeType: 3
+      });
+
+      this.foodsScroll.on("scroll", pos => {
+        // not very understand pos mean, too late, figure it out tomorrow
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+    },
+    _calculateHeight() {
+      let foodList = this.$refs.foodList;
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixin';
+
+.goods {
+  display: flex;
+  position: absolute;
+  top: 174px;
+  bottom: 46px;
+  width: 100%;
+  overflow: hidden;
+
+  .menu-wrapper {
+    flex: 0 0 80px;
+    width: 80px;
+    background: #f3f5f7;
+
+    .menu-item {
+      display: table;
+      height: 54px;
+      width: 56px;
+      padding: 0 12px;
+      line-height: 14px;
+
+      &.current {
+        position: relative;
+        z-index: 10;
+        margin-top: -1px;
+        background: #fff;
+        font-weight: 700;
+
+        .text {
+          border-nono();
+        }
+      }
+
+      .icon {
+        publ-icon(3, 12px, 2px);
+      }
+
+      .text {
+        display: table-cell;
+        width: 56px;
+        vertical-align: middle;
+        border-1px(rgba(7, 17, 27, 0.1));
+        font-size: 12px;
+      }
+    }
+  }
+
+  .foods-wrapper {
+    flex: 1;
+
+    .title {
+      padding-left: 14px;
+      height: 26px;
+      line-height: 26px;
+      font-size: 12px;
+      color: rgb(147, 153, 159);
+      border-left: 2px solid #d9dde1;
+      background: #f3f5f7;
+    }
+
+    .food-item {
+      display: flex;
+      margin: 18px;
+      padding-bottom: 18px;
+      border-1px(rgba(7, 17, 27, 0.1));
+
+      &:last-child {
+        border-none();
+        margin-bottom: 0;
+      }
+
+      .icon {
+        flex: 0 0 57px;
+        margin-right: 10px;
+      }
+
+      .content {
+        flex: 1;
+
+        .name {
+          margin: 2px 0 8px 0;
+          height: 14px;
+          font-size: 14px;
+          line-height: 14px;
+          color: rgb(7, 17, 27);
+        }
+
+        .desc, .extra {
+          font-size: 10px;
+          color: rgb(147, 153, 159);
+        }
+
+        .desc {
+          line-height: 12px;
+          margin-bottom: 8px;
+        }
+
+        .extra {
+          line-height: 10px;
+
+          .count {
+            margin-right: 12px;
           }
         }
-        return 0
-      }
-    },
-    created() {
-      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
 
-      this.$http.get('/api/goods').then((response) => {
-        response = response.body
-        if (response.errno === ERR_OK) {
-          this.goods = response.data
-          this.$nextTick(() => {
-            this._initScroll()
-            this._calculateHeight()
-          })
-        }
-      })
-    },
-    methods: {// when I create at the first time, I typed "method", then browser keep complain about "_initScroll is not a function"
-      selectMenu(index, event) {
-        if (!event._constructed) {
-          return
-        }
-        let foodList = this.$refs.foodList
-        let el = foodList[index]
-        this.foodsScroll.scrollToElement(el, 300)
-      },
-      _initScroll() {
-        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
-          click: true
-        })
-        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
-          click: true,
-          probeType: 3
-        })
+        .price {
+          font-weight: 700;
+          line-height: 24px;
 
-        this.foodsScroll.on('scroll', (pos) => { // not very understand pos mean, too late, figure it out tomorrow
-          this.scrollY = Math.abs(Math.round(pos.y))
-        })
-      },
-      _calculateHeight() {
-        let foodList = this.$refs.foodList
-        let height = 0
-        this.listHeight.push(height)
-        for (let i = 0; i < foodList.length; i++) {
-          let item = foodList[i]
-          height += item.clientHeight
-          this.listHeight.push(height)
+          .money-sign, .new {
+            color: rgb(240, 20, 20);
+          }
+
+          .money-sign, .old {
+            font-size: 10px;
+          }
+
+          .new {
+            margin-right: 8px;
+            font-size: 14px;
+          }
+
+          .old {
+            text-decoration: line-through;
+            color: rgb(147, 153, 159);
+          }
         }
       }
     }
   }
-</script>
-
-<style lang="stylus" rel="stylesheet/stylus">
-  @import "../../common/stylus/mixin"
-
-  .goods
-    display: flex
-    position: absolute
-    top: 174px
-    bottom: 46px
-    width: 100%
-    overflow: hidden
-    .menu-wrapper
-      flex: 0 0 80px
-      width: 80px
-      background: #f3f5f7
-      .menu-item
-        display: table
-        height: 54px
-        width: 56px
-        padding: 0 12px
-        line-height: 14px
-        &.current
-          position: relative
-          z-index: 10
-          margin-top: -1px
-          background: #fff
-          font-weight: 700
-          .text
-            border-nono()
-        .icon
-          publ-icon(3, 12px, 2px)
-        .text
-          display: table-cell
-          width: 56px
-          vertical-align: middle
-          border-1px(rgba(7, 17, 27, 0.1))
-          font-size: 12px
-    .foods-wrapper
-      flex: 1
-      .title
-        padding-left: 14px
-        height: 26px
-        line-height: 26px
-        font-size: 12px
-        color: rgb(147, 153, 159)
-        border-left: 2px solid #d9dde1
-        background: #f3f5f7
-      .food-item
-        display: flex
-        margin: 18px
-        padding-bottom: 18px
-        border-1px(rgba(7, 17, 27, 0.1))
-        &:last-child
-          border-none()
-          margin-bottom: 0
-        .icon
-          flex: 0 0 57px
-          margin-right: 10px
-        .content
-          flex: 1
-          .name
-            margin: 2px 0 8px 0
-            height: 14px
-            font-size: 14px
-            line-height: 14px
-            color: rgb(7, 17, 27)
-          .desc, .extra
-            font-size: 10px
-            color: rgb(147, 153, 159)
-          .desc
-            line-height: 12px
-            margin-bottom: 8px
-          .extra
-            line-height: 10px
-            .count
-              margin-right: 12px
-          .price
-            font-weight: 700
-            line-height: 24px
-            .money-sign, .new
-              color: rgb(240, 20, 20)
-            .money-sign, .old
-              font-size: 10px
-            .new
-              margin-right: 8px
-              font-size: 14px
-            .old
-              text-decoration: line-through
-              color: rgb(147, 153, 159)
+}
 </style>
